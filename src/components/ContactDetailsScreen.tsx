9a152e8f11d5aed7aa2a7027d8258d02072bde2f
hari kishan veeranki <hk54531@gmail.com>
2024-03-11 19:19:51 +0530

    add checkboxes

--- src/components/ContactDetailsScreen.tsx
+++ src/components/ContactDetailsScreen.tsx
@@ -7,15 +7,32 @@ import {
   Image,
   StatusBar,
   TouchableOpacity,
+  Share,
+  PermissionsAndroid,
+  Platform,
+  ToastAndroid,
+  Alert,
 } from 'react-native';
 import Contacts, {Contact} from 'react-native-contacts';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import RNFS from 'react-native-fs';
+import {VCard4} from 'vcard4-ts';
+
+import {
+  responsiveHeight,
+  responsiveScreenFontSize,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
 
 const ContactDetailsScreen = ({route, navigation}: any) => {
-  const [fetchedContact, setFetchedContact] = useState<Contact>(null);
+  const [fetchedContact, setFetchedContact] = useState<any>(null);
+  const [checkbox, setCheckbox] = useState<boolean>(false);
+  const [checkboxEmail, setCheckboxEmail] = useState<boolean>(false);
+  const [checkedSocialProfiles, setCheckedSocialProfiles] = useState<any>({});
 
   useEffect(() => {
     const fetchContactDetails = async () => {
@@ -28,7 +45,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
               label: 'twitter',
               value: {
                 urlString: 'http://twitter.com/HankyPanky',
-                username: 'HankyPanky',
+                username: 'hari twitter',
                 service: 'Twitter',
               },
             },
@@ -36,7 +53,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
               label: 'facebook',
               value: {
                 urlString: 'http://www.facebook.com/HankZakoff',
-                username: 'HankZakoff',
+                username: 'god ryzen',
                 service: 'Facebook',
               },
             },
@@ -44,7 +61,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
               label: 'instagram',
               value: {
                 urlString: 'http://www.instagram.com/HankZakoff',
-                username: 'HankZakoff',
+                username: 'rocky',
                 service: 'Instagram',
               },
             },
@@ -52,7 +69,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
               label: 'linkedin',
               value: {
                 urlString: 'http://www.linkedin.com/HankZakoff',
-                username: 'HankZakoff',
+                username: 'Hank Zakoff',
                 service: 'LinkedIn',
               },
             },
@@ -60,6 +77,12 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
         };
         setFetchedContact(updatedContact);
         console.log(JSON.stringify(updatedContact), '???');
+        const initialCheckedState: any = {};
+        updatedContact.socialProfiles.forEach((profile: any) => {
+          initialCheckedState[profile.label] = false;
+        });
+        setCheckedSocialProfiles(initialCheckedState);
+        console.log(checkedSocialProfiles, '???');
       } catch (error) {
         console.error('Error fetching contact details:', error);
       }
@@ -67,6 +90,131 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
 
     fetchContactDetails();
   }, []);
+  const handleCheckBoxChange = (label: string) => {
+    setCheckedSocialProfiles((prevState: any) => ({
+      ...prevState,
+      [label]: !prevState[label],
+    }));
+  };
+
+  //   const shareContact = () => {
+  //     let message = `Full Name: ${fetchedContact.givenName} ${fetchedContact.middleName} ${fetchedContact.familyName}\n`;
+  //     if (fetchedContact.jobTitle) {
+  //       message += `Job Role: ${fetchedContact.jobTitle}\n`;
+  //     }
+  //     if (fetchedContact.company) {
+  //       message += `Company Name: ${fetchedContact.company}\n`;
+  //     }
+  //     if (checkbox) {
+  //       message += 'Phone Numbers:\n';
+  //       fetchedContact.phoneNumbers.forEach((phoneNumber: any) => {
+  //         message += ` - ${phoneNumber.number}\n`;
+  //       });
+  //     }
+  //     if (checkboxEmail) {
+  //       message += 'Email Addresses:\n';
+  //       fetchedContact.emailAddresses.forEach((email: any) => {
+  //         message += ` - ${email.email}\n`;
+  //       });
+  //     }
+  //     // Add selected social profiles if any
+  //     const selectedProfiles = Object.entries(checkedSocialProfiles)
+  //       .filter(([_, value]) => value)
+  //       .map(([key, _]) => {
+  //         const profile = fetchedContact.socialProfiles.find(
+  //           (p: any) => p.label === key,
+  //         );
+  //         if (profile) {
+  //           return ` - ${profile.value.service}: ${profile.value.username}\n`;
+  //         }
+  //         return '';
+  //       })
+  //       .join('');
+
+  //     if (selectedProfiles) {
+  //       message += 'Social Profiles:\n';
+  //       message += selectedProfiles;
+  //     }
+
+  //     // Share the constructed message
+  //     console.log(message, 'vcard details');
+  //     Share.share({
+  //       message: message,
+  //     });
+  //   };
+
+  //vcard generator
+  const shareContact = () => {
+    if (
+      !checkbox &&
+      !checkboxEmail &&
+      Object.values(checkedSocialProfiles).every(value => !value)
+    ) {
+      // Show an alert to select anything
+      Alert.alert('Please select at least one contact detail to share.');
+      return; // Exit the function
+    }
+    // Construct the VCard string
+    let vCard = `BEGIN:VCARD\nVERSION:3.0\n`;
+
+    // Add contact details
+    vCard += `FN:${fetchedContact.givenName} ${fetchedContact.middleName} ${fetchedContact.familyName}\n`;
+    if (fetchedContact.jobTitle) {
+      vCard += `TITLE:${fetchedContact.jobTitle}\n`;
+    }
+    if (fetchedContact.company) {
+      vCard += `ORG:${fetchedContact.company}\n`;
+    }
+
+    if (checkbox) {
+      fetchedContact.phoneNumbers.forEach((phoneNumber: any) => {
+        vCard += `TEL;TYPE=CELL:${phoneNumber.number}\n`;
+      });
+    }
+    if (checkboxEmail) {
+      fetchedContact.emailAddresses.forEach((email: any) => {
+        vCard += `EMAIL:${email.email}\n`;
+      });
+    }
+
+    Object.entries(checkedSocialProfiles)
+      .filter(([_, value]) => value)
+      .forEach(([key, _]) => {
+        const profile = fetchedContact.socialProfiles.find(
+          (p: any) => p.label === key,
+        );
+        if (profile) {
+          vCard += `X-SOCIALPROFILE;TYPE=${profile.label.toUpperCase()}:http://${
+            profile.value.username
+          }\n`;
+        }
+      });
+
+    // Close the VCard string
+    vCard += `END:VCARD`;
+
+    const path = RNFS.DocumentDirectoryPath + `/contact.vcf`;
+    RNFS.writeFile(path, vCard, 'utf8')
+      .then(async () => {
+        console.log('VCF file saved successfully:', path);
+        // Optionally, you can share the file here
+        // Share the VCard file
+        await Share.share({
+          title: 'Share Contact',
+          url: 'file://' + path,
+          //   type: 'text/vcard', // or 'text/x-vcard'
+        });
+      })
+      .catch(error => {
+        console.error('Error saving VCF file:', error);
+      });
+
+    console.log(vCard);
+    // Share the VCard
+    // Share.share({
+    //   message: vCard,
+    // });
+  };
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
@@ -134,6 +282,48 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
 
                     marginBottom: 10,
                   }}>
+                  <TouchableOpacity
+                    style={
+                      checkbox
+                        ? {
+                            borderWidth: 2,
+                            borderColor: '#8e4ede',
+                            borderRadius: 5,
+                            backgroundColor: '#8e4ede',
+                            // color: 'white',
+                            width: responsiveHeight(2.7),
+                            height: responsiveHeight(2.7),
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginRight: responsiveWidth(2),
+                            marginTop: responsiveHeight(0.3),
+                            // marginLeft: responsiveWidth(6),
+                          }
+                        : {
+                            padding: 5,
+                            paddingRight: 3,
+                            borderWidth: 2,
+                            borderColor: '#8e4ede',
+                            borderRadius: 5,
+                            // color: 'white',
+                            width: responsiveHeight(2.7),
+                            height: responsiveHeight(2.7),
+                            marginRight: responsiveWidth(2),
+                            marginTop: responsiveHeight(0.3),
+                            // marginLeft: responsiveWidth(6),
+                          }
+                    }
+                    testID={`checkbox`}
+                    onPress={() => {
+                      // this.removecheckBoxErr();
+                      setCheckbox(!checkbox);
+                    }}>
+                    <Icon
+                      name={checkbox ? 'check' : ''}
+                      size={16}
+                      color={checkbox ? 'white' : 'transparent'}
+                    />
+                  </TouchableOpacity>
                   <MaterialIcons
                     name="wifi-calling-3"
                     size={25}
@@ -176,6 +366,48 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
                     alignItems: 'center',
                     marginBottom: 10,
                   }}>
+                  <TouchableOpacity
+                    style={
+                      checkboxEmail
+                        ? {
+                            borderWidth: 2,
+                            borderColor: '#8e4ede',
+                            borderRadius: 5,
+                            backgroundColor: '#8e4ede',
+                            // color: 'white',
+                            width: responsiveHeight(2.7),
+                            height: responsiveHeight(2.7),
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginRight: responsiveWidth(2),
+                            marginTop: responsiveHeight(0.3),
+                            // marginLeft: responsiveWidth(6),
+                          }
+                        : {
+                            padding: 5,
+                            paddingRight: 3,
+                            borderWidth: 2,
+                            borderColor: '#8e4ede',
+                            borderRadius: 5,
+                            // color: 'white',
+                            width: responsiveHeight(2.7),
+                            height: responsiveHeight(2.7),
+                            marginRight: responsiveWidth(2),
+                            marginTop: responsiveHeight(0.3),
+                            // marginLeft: responsiveWidth(6),
+                          }
+                    }
+                    testID={`checkboxEmail`}
+                    onPress={() => {
+                      // this.removecheckBoxErr();
+                      setCheckboxEmail(!checkboxEmail);
+                    }}>
+                    <Icon
+                      name={checkboxEmail ? 'check' : ''}
+                      size={16}
+                      color={checkboxEmail ? 'white' : 'transparent'}
+                    />
+                  </TouchableOpacity>
                   <Feather name="mail" size={25} color={'black'} />
                   <View>
                     <Text
@@ -207,7 +439,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
                   </TouchableOpacity>
                 </View>
               )}
-              {fetchedContact.socialProfiles?.map(
+              {/* {fetchedContact.socialProfiles?.map(
                 (profile: any, index: any) => (
                   <View
                     key={index}
@@ -230,7 +462,7 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
                     )}
                     {/* {profile.label && (
                       <Feather name={profile.label} size={25} color={'black'} />
-                    )} */}
+                    )} 
                     <View>
                       <Text
                         style={{
@@ -253,10 +485,104 @@ const ContactDetailsScreen = ({route, navigation}: any) => {
                     </TouchableOpacity>
                   </View>
                 ),
+              )} */}
+              {/* </View>
+          </View>
+        )} */}
+              {fetchedContact.socialProfiles?.map(
+                (profile: any, index: any) => (
+                  <View
+                    key={index}
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      marginBottom: 10,
+                    }}>
+                    <TouchableOpacity
+                      style={{
+                        borderWidth: 2,
+                        borderColor: '#8e4ede',
+                        borderRadius: 5,
+                        backgroundColor: checkedSocialProfiles[profile.label]
+                          ? '#8e4ede'
+                          : 'transparent',
+                        width: 20,
+                        height: 20,
+                        marginRight: 10,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                      onPress={() => handleCheckBoxChange(profile.label)}>
+                      {checkedSocialProfiles[profile.label] && (
+                        <Icon name="check" size={14} color="white" />
+                      )}
+                    </TouchableOpacity>
+                    {/* Render social profile icon based on label */}
+                    {profile.label === 'twitter' && (
+                      <Feather name="twitter" size={25} color={'#1DA1F2'} />
+                    )}
+                    {profile.label === 'facebook' && (
+                      <Entypo name="facebook" size={25} color={'#4267B2'} />
+                    )}
+                    {profile.label === 'instagram' && (
+                      <Feather name="instagram" size={25} color={'#C13584'} />
+                    )}
+                    {profile.label === 'linkedin' && (
+                      <Feather name="linkedin" size={25} color={'#0077B5'} />
+                    )}
+                    <View>
+                      <Text
+                        style={{
+                          fontSize: 18,
+                          fontWeight: '500',
+                          marginLeft: 10,
+                        }}>
+                        {profile.value.service}
+                      </Text>
+                      <Text style={{marginLeft: 10, fontSize: 14}}>
+                        {profile.value.username}
+                      </Text>
+                    </View>
+                    <TouchableOpacity style={{marginLeft: 'auto'}}>
+                      <Entypo
+                        name="dots-three-vertical"
+                        size={20}
+                        color={'grey'}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                ),
               )}
             </View>
           </View>
         )}
+
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: '80%',
+            backgroundColor: '#EFE3FE',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            alignSelf: 'center',
+          }}>
+          <Text>Cancel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={shareContact}
+          style={{
+            height: 50,
+            width: '80%',
+            backgroundColor: '#8e4ede',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            alignSelf: 'center',
+            marginVertical: 10,
+          }}>
+          <Text style={{color: 'white'}}>Share</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
